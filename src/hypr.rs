66a4d4038412e9a71158f8e4e7cea47ab549a9c1
Joshua <joshua.c.stalder@gmail.com>
2023-09-07 23:01:34 +0200

    feat: hyprland sockets and workspace subcommand

---
+++ src/hypr.rs
@@ -0,0 +1,62 @@
+use std::io::{Read, Write};
+use std::os::unix::net::UnixStream;
+use anyhow::Context;
+use serde_json::Value;
+
+/// Opens socket two for reading
+pub fn open_events() -> anyhow::Result<UnixStream> {
+    UnixStream::connect(get_hypr_socket("socket2")?).context("failed to open socket 2")
+}
+
+/// Reads a list of new events off of socket 2, returns an empty list if socket is closed
+pub fn read_events(socket: &mut UnixStream) -> anyhow::Result<Vec<(String, Vec<String>)>> {
+    // Yes, if there is data on the socket > than 256 bytes, things begin to break.
+    let mut buf = [0; 256];
+
+    let len = socket.read(&mut buf).context("failed to read from socket 2")?;
+
+    Ok(String::from_utf8(buf[..len].to_vec()).context("socket 2 did not return valid utf-8")?
+        .split('\n').filter(|s| !s.is_empty()).filter_map(|s| {
+
+        // Parses e.g. activewindow>>alacritty,Window Title
+        let splits: Vec<&str> = s.split(">>").collect();
+        if splits.is_empty() { return None }
+
+        let name = splits.first().expect("vec behaving weirdly").to_string();
+        let args = if let Some(args) = splits.get(1) {
+            args.split(',').map(String::from).collect()
+        } else { vec![] };
+
+        Some((name, args))
+    }).collect())
+}
+
+
+
+/// Gets information from socket 1, is always executed through a batch request and returned in json
+pub fn get_info(requests: Vec<String>) -> anyhow::Result<Vec<Value>>{
+    // Produces request string, e.g. [[BATCH]] j/monitors ; j/workspaces
+    let request = "[[BATCH]] ".to_string() + &requests.iter().map(|s| "j/".to_string() + s).collect::<Vec<String>>().join(" ; ");
+
+    let mut socket = UnixStream::connect(get_hypr_socket("socket")?).context("failed to open socket 1")?;
+    socket.write_all(request.as_bytes()).context("failed to write to socket 1")?;
+
+    let mut response = String::new();
+    socket.read_to_string(&mut response).context("failed to read from socket 1")?;
+
+    // Response will be everything concatenated without any spaces, put DEL character between these so we can split by that
+    response = response.replace("][", "]\x7f[").replace("}[", "}\x7f[").replace("]{", "]\x7f{");
+
+    // Now split by that inserted character
+    response.split('\x7f')
+        .map(serde_json::from_str::<Value>)
+        .collect::<serde_json::Result<Vec<Value>>>().context("socket 1 did not return valid json")
+}
+
+/// Returns the path to a socket, based on its name (without . and ending) and the instance signature
+pub fn get_hypr_socket(name: &str) -> anyhow::Result<String> {
+    let instance = std::env::var("HYPRLAND_INSTANCE_SIGNATURE").context("couldn't find instance singature, is hyprland running?")?;
+
+    Ok(format!("/tmp/hypr/{instance}/.{name}.sock"))
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,146 @@
-fn main() {
-    println!("Hello, world!");
+mod hypr;
+
+use std::collections::HashMap;
+use anyhow::{anyhow, Context};
+use clap::{Parser, Subcommand};
+use serde_json::Value;
+use crate::hypr::{get_info, open_events, read_events};
+
+const MONITOR_EVENTS: [&str; 3] = ["focusedmon", "monitorremoved", "monitoradded"];
+const WORKSPACE_EVENTS: [&str; 7] = ["focusedmon", "monitorremoved", "monitoradded", "workspace", "createworkspace", "destroyworkspace", "moveworkspace"];
+
+#[derive(Parser)]
+#[clap(version, about)]
+#[command(disable_help_subcommand = true)]
+struct Command {
+    /// What to watch
+    #[clap(subcommand)]
+    pub what: Type,
+    /// Run specified command on shell on change, with data in $data
+    #[clap(short, long)]
+    pub run: Option<String>,
+    /// Query only once, don't listen for events
+    #[clap(short, long)]
+    pub once: bool,
 }
+
+#[derive(Subcommand)]
+enum Type {
+    /// Watch changes in monitors
+    Monitors,
+    /// Watch changes in workspaces
+    Workspaces {
+        /// Only watch workspaces on monitor
+        #[clap(short, long)]
+        monitor: Option<String>,
+        /// Also watch workspaces, which are empty and only defined in the config
+        #[clap(short, long)]
+        persistent: bool
+    },
+    /// Watch changes in clients (windows)
+    Clients {
+        /// Only watch clients on monitor
+        #[clap(short, long)]
+        monitor: Option<String>,
+        /// Only watch clients on workspace
+        #[clap(short, long)]
+        workspace: Option<String>
+    }
+}
+
+fn main() {
+    let command = Command::parse();
+
+    // One-shot
+    if command.once {
+        let result = match command.what {
+            Type::Monitors => { Err(anyhow!("not yet implemented")) }
+            Type::Workspaces { persistent, monitor} => { prepare_workspaces(monitor) }
+            Type::Clients { .. } => { Err(anyhow!("not yet implemented")) }
+        };
+
+        match result {
+            Ok(s) => { println!("{s}") }
+            Err(e) => { eprintln!("{e}")}
+        }
+
+        return;
+    }
+
+    // Listen
+    let mut socket = open_events().unwrap();
+
+    loop {
+        let events = match read_events(&mut socket) {
+            Ok(e) => {
+                if e.is_empty() {
+                    eprintln!("hyprland event socket has closed");
+                    return;
+                } else { e }
+            }
+            Err(e) => {
+                eprintln!("{e}");
+                continue;
+            }
+        };
+
+        let mut result = None;
+        for (name, _) in events {
+            result = match &command.what {
+                Type::Monitors => { None }
+                Type::Workspaces { monitor, persistent } => {
+                    if WORKSPACE_EVENTS.contains(&name.as_str()) { Some(prepare_workspaces(monitor.clone())) }
+                    else { None }
+                }
+                Type::Clients { .. } => { None }
+            };
+
+            if result.is_some() { break; }
+        }
+
+        if let Some(result) = result {
+            match result {
+                Ok(s) => { println!("{s}") }
+                Err(e) => { eprintln!("{e}")}
+            }
+        }
+    }
+}
+
+/// Prepares the workspace data
+/// In addition to the workspaces, it will also query the monitor data to see whether the workspace is displayed and focussed
+fn prepare_workspaces(on_monitor: Option<String>) -> anyhow::Result<String> {
+    let mut data = get_info(vec!("workspaces".into(), "monitors".into()))?;
+
+    // Process monitors to retrieve shown and active workspaces
+    let mut shown_map = HashMap::new();
+    let monitors = data.get(1).unwrap();
+    for monitor in monitors.as_array().unwrap() {
+        shown_map.insert(
+                monitor.get("activeWorkspace").and_then(|p| p.get("id")).and_then(Value::as_u64).unwrap(),
+                    monitor.get("focused").and_then(Value::as_bool).unwrap());
+    }
+
+    let mut body = data.remove(0);
+    if let Value::Array(workspaces) = &mut body {
+        // Remove workspaces not on monitor
+        if let Some(monitor) = on_monitor {
+            workspaces.retain(|w| {
+                w.get("monitor").and_then(Value::as_str).unwrap() == monitor
+            })
+        }
+
+        // Add custom attributes
+        for workspace in workspaces {
+            if let Value::Object(map) = workspace {
+                let id = &map.get("id").and_then(Value::as_u64).unwrap();
+
+                map.insert("shown".into(), Value::Bool(shown_map.get(id).is_some()));
+                map.insert("active".into(), Value::Bool(*shown_map.get(id).unwrap_or(&false)));
+            }
+        }
+    }
+
+    serde_json::to_string(&body).context("failed to re-serialize to json")
+}
+
