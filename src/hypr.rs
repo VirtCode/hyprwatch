3a414fae1a57576c29af66bab7abb24d24996ec3
Joshua <joshua.c.stalder@gmail.com>
2023-09-11 22:07:04 +0200

    feat: loading workspaces from config for persistent display

--- src/hypr.rs
+++ src/hypr.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
 use std::io::{Read, Write};
 use std::os::unix::net::UnixStream;
-use anyhow::Context;
+use std::str::FromStr;
+use anyhow::{anyhow, Context};
 use serde_json::Value;
 
 /// Opens socket two for reading
@@ -31,8 +35,6 @@ pub fn read_events(socket: &mut UnixStream) -> anyhow::Result<Vec<(String, Vec<S
     }).collect())
 }
 
-
-
 /// Gets information from socket 1, is always executed through a batch request and returned in json
 pub fn get_info(requests: Vec<String>) -> anyhow::Result<Vec<Value>>{
     // Produces request string, e.g. [[BATCH]] j/monitors ; j/workspaces
@@ -51,6 +53,54 @@ pub fn get_info(requests: Vec<String>) -> anyhow::Result<Vec<Value>>{
     response.split('\x7f')
         .map(serde_json::from_str::<Value>)
         .collect::<serde_json::Result<Vec<Value>>>().context("socket 1 did not return valid json")
+}
+
+// Holds required information for a workspace defined in the config file
+#[derive(Default, Debug)]
+pub struct WorkspaceInformation {
+    pub id: Option<u64>,
+    pub name: Option<String>,
+    pub monitor: Option<String>
+}
+
+// Reads the hyprland configured workspaces and returns a vector of information
+pub fn get_config_workspaces() -> anyhow::Result<Vec<WorkspaceInformation>> {
+    let mut file = File::open(get_hypr_config()?).context("couldn't open hyprland config")?;
+
+    let mut content = String::new();
+    file.read_to_string(&mut content).context("failed to read from hyprland config file")?;
+
+    let definitions = content.split('\n')
+        .filter(|line| line.trim().starts_with("workspace ") || line.trim().starts_with("workspace=")); // Ugly, but workspace_swipe shall not be matched
+
+    let mut infos = vec![];
+    for line in definitions {
+        let attributes = line.find("=").map(|i| line[(i+1)..].trim()).context("couldn't find '=' in workspace definition")?;
+
+        let mut info = WorkspaceInformation::default();
+        for (i, attribute) in attributes.split(',').enumerate() {
+            let attribute = attribute.trim();
+
+            if i == 0 {
+                if attribute.starts_with("name:") { info.name = Some(attribute[("name:".len())..].to_owned()) }
+                else { info.id = Some(u64::from_str(attribute).context("id of workspace is not integer")?) }
+            } else if attribute.starts_with("monitor:") {
+                info.monitor = Some(attribute[("monitor:".len())..].to_owned())
+            }
+        }
+
+        infos.push(info);
+
+    }
+
+    Ok(infos)
+}
+
+/// Returns the path to the hyprland config file
+pub fn get_hypr_config() -> anyhow::Result<String> {
+    env::var("XDG_CONFIG_HOME")
+        .or_else(|_e| env::var("HOME").map(|s| s + "/.config"))
+        .map(|s| s + "/hypr/hyprland.conf").context("$HOME is not set, cannot find hyprland config")
 }
 
 /// Returns the path to a socket, based on its name (without . and ending) and the instance signature
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::collections::HashMap;
 use anyhow::{anyhow, Context};
 use clap::{Parser, Subcommand};
 use serde_json::Value;
-use crate::hypr::{get_info, open_events, read_events};
+use serde_json::value::Index;
+use crate::hypr::{get_config_workspaces, get_info, open_events, read_events, WorkspaceInformation};
 
 const MONITOR_EVENTS: [&str; 3] = ["focusedmon", "monitorremoved", "monitoradded"];
 const WORKSPACE_EVENTS: [&str; 7] = ["focusedmon", "monitorremoved", "monitoradded", "workspace", "createworkspace", "destroyworkspace", "moveworkspace"];
@@ -22,6 +23,9 @@ struct Command {
     /// Query only once, don't listen for events
     #[clap(short, long)]
     pub once: bool,
+    /// Pretty print result (uses multiple lines per event)
+    #[clap(short, long)]
+    pub pretty: bool,
 }
 
 #[derive(Subcommand)]
@@ -35,7 +39,7 @@ enum Type {
         monitor: Option<String>,
         /// Also watch workspaces, which are empty and only defined in the config
         #[clap(short, long)]
-        persistent: bool
+        config: bool,
     },
     /// Watch changes in clients (windows)
     Clients {
@@ -44,28 +48,41 @@ enum Type {
         monitor: Option<String>,
         /// Only watch clients on workspace
         #[clap(short, long)]
-        workspace: Option<String>
-    }
+        workspace: Option<String>,
+    },
 }
 
 fn main() {
     let command = Command::parse();
 
-    // One-shot
-    if command.once {
-        let result = match command.what {
+    // Load persistent stuff if required
+    let config_workspaces = if let Type::Workspaces { config, .. } = &command.what {
+        if *config {
+            match get_config_workspaces() {
+                Ok(w) => { Some(w) }
+                Err(e) => {
+                    eprintln!("{e}");
+                    return;
+                }
+            }
+        } else { None }
+    } else { None };
+
+    // Eventless
+    {
+        let result = match &command.what {
             Type::Monitors => { Err(anyhow!("not yet implemented")) }
-            Type::Workspaces { persistent, monitor} => { prepare_workspaces(monitor) }
+            Type::Workspaces { monitor, .. } => { prepare_workspaces(monitor, &config_workspaces, command.pretty) }
             Type::Clients { .. } => { Err(anyhow!("not yet implemented")) }
         };
 
         match result {
             Ok(s) => { println!("{s}") }
-            Err(e) => { eprintln!("{e}")}
+            Err(e) => { eprintln!("{e}") }
         }
-
-        return;
     }
+
+    if command.once { return; }
 
     // Listen
     let mut socket = open_events().unwrap();
@@ -88,9 +105,8 @@ fn main() {
         for (name, _) in events {
             result = match &command.what {
                 Type::Monitors => { None }
-                Type::Workspaces { monitor, persistent } => {
-                    if WORKSPACE_EVENTS.contains(&name.as_str()) { Some(prepare_workspaces(monitor.clone())) }
-                    else { None }
+                Type::Workspaces { monitor, .. } => {
+                    if WORKSPACE_EVENTS.contains(&name.as_str()) { Some(prepare_workspaces(&monitor, &config_workspaces, command.pretty)) } else { None }
                 }
                 Type::Clients { .. } => { None }
             };
@@ -101,7 +117,7 @@ fn main() {
         if let Some(result) = result {
             match result {
                 Ok(s) => { println!("{s}") }
-                Err(e) => { eprintln!("{e}")}
+                Err(e) => { eprintln!("{e}") }
             }
         }
     }
@@ -109,7 +125,7 @@ fn main() {
 
 /// Prepares the workspace data
 /// In addition to the workspaces, it will also query the monitor data to see whether the workspace is displayed and focussed
-fn prepare_workspaces(on_monitor: Option<String>) -> anyhow::Result<String> {
+fn prepare_workspaces(on_monitor: &Option<String>, config_workspaces: &Option<Vec<WorkspaceInformation>>, pretty: bool) -> anyhow::Result<String> {
     let mut data = get_info(vec!("workspaces".into(), "monitors".into()))?;
 
     // Process monitors to retrieve shown and active workspaces
@@ -117,30 +133,69 @@ fn prepare_workspaces(on_monitor: Option<String>) -> anyhow::Result<String> {
     let monitors = data.get(1).unwrap();
     for monitor in monitors.as_array().unwrap() {
         shown_map.insert(
-                monitor.get("activeWorkspace").and_then(|p| p.get("id")).and_then(Value::as_u64).unwrap(),
-                    monitor.get("focused").and_then(Value::as_bool).unwrap());
+            monitor.get("activeWorkspace").and_then(|p| p.get("id")).and_then(Value::as_u64).unwrap(),
+            monitor.get("focused").and_then(Value::as_bool).unwrap());
     }
 
     let mut body = data.remove(0);
     if let Value::Array(workspaces) = &mut body {
         // Remove workspaces not on monitor
-        if let Some(monitor) = on_monitor {
+        if let Some(monitor) = &on_monitor {
             workspaces.retain(|w| {
                 w.get("monitor").and_then(Value::as_str).unwrap() == monitor
             })
         }
 
-        // Add custom attributes
-        for workspace in workspaces {
-            if let Value::Object(map) = workspace {
-                let id = &map.get("id").and_then(Value::as_u64).unwrap();
+        let mut existing = vec![];
 
-                map.insert("shown".into(), Value::Bool(shown_map.get(id).is_some()));
-                map.insert("active".into(), Value::Bool(*shown_map.get(id).unwrap_or(&false)));
+        // Add custom attributes
+        for workspace in workspaces.iter_mut() {
+            if let Value::Object(map) = workspace {
+                let id = map.get("id").and_then(Value::as_u64).unwrap();
+                let name = map.get("name").and_then(Value::as_str).unwrap().to_owned();
+
+                map.insert("shown".into(), Value::Bool(shown_map.get(&id).is_some()));
+                map.insert("active".into(), Value::Bool(*shown_map.get(&id).unwrap_or(&false)));
+                map.insert("exists".into(), Value::Bool(true));
+
+                if let Some(infos) = &config_workspaces {
+                    let configured = infos.iter().position(|info| info.id == Some(id) || info.name == Some(name.to_owned()));
+                    map.insert("dynamic".into(), Value::Bool(!configured.is_some()));
+                    if let Some(index) = configured { existing.push(index) }
+                }
             }
         }
+
+        if let Some(infos) = &config_workspaces {
+            workspaces.append(&mut infos.iter().enumerate()
+                .filter(|(i, info)| {
+                    !existing.contains(i) &&
+                        (!on_monitor.is_some() || on_monitor == &info.monitor)
+                }).map(|(_, info)| {
+                    let mut map = serde_json::Map::new();
+
+                    if let Some(id) = info.id { map.insert("id".to_string(), Value::Number(id.into())); }
+                    if let Some(name) = &info.name { map.insert("name".to_string(), Value::String(name.to_owned())); }
+                    if let Some(monitor) = &info.monitor { map.insert("monitor".to_string(), Value::String(monitor.to_owned())); }
+
+                    map.insert("dynamic".to_string(), Value::Bool(false));
+                    map.insert("exists".to_string(), Value::Bool(false));
+
+                    Value::Object(map.into())
+                }).collect());
+        }
+
+        workspaces.sort_by(|w1, w2| {
+            w1.get("id").and_then(|v| v.as_u64()).unwrap_or(u64::MAX)
+                .cmp(&w2.get("id").and_then(|v| v.as_u64()).unwrap_or(u64::MAX))
+        })
     }
 
-    serde_json::to_string(&body).context("failed to re-serialize to json")
+    if pretty {
+        serde_json::to_string_pretty(&body).context("failed to re-serialize to json")
+    } else {
+        serde_json::to_string(&body).context("failed to re-serialize to json")
+    }
+
 }
 
